12ef5c38b09318100f001de27a2422c5e3574b62
ofnullable <ofnullable@gmail.com>
2019-12-18 23:43:13 +0900

    :bookmark: Publish to npm

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './Button/Button';
 export { default as ButtonGroup } from './ButtonGroup/ButtonGroup';
 export { default as Dialog } from './Dialog/Dialog';
 export { default as Icon } from './Icon/Icon';
+export { default as Input } from './Inputs/Input';
